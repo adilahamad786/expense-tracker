f84ac90554672c2a821bd718e1a69435fb29ceb3
adilahamad786 <adilahamad.personally@gmail.com>
2022-09-13 17:19:28 +0530

    Expense Items related components ready!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Expense from "./components/Expense";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expense/>
     </div>
   );
 }
---
+++ src/components/Expense.jsx
@@ -0,0 +1,21 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expense.css"
+
+const expense = [
+  {title: "Car Insurance", cost: 786, date: new Date(2021, 5, 24)},
+  {title: "Laptop", cost: 832, date: new Date(2020, 2, 5)},
+  {title: "Bike", cost: 1542, date: new Date(2022, 6, 29)},
+]
+
+function Expense() {
+  return (
+    <Card className="expense">
+      <ExpenseItem title={expense[0].title} amount={expense[0].cost} date={expense[0].date}></ExpenseItem>
+      <ExpenseItem title={expense[1].title} amount={expense[1].cost} date={expense[1].date}></ExpenseItem>
+      <ExpenseItem title={expense[2].title} amount={expense[2].cost} date={expense[2].date}></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expense;
